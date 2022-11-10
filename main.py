04631d7e85615bf8e50c9b72f294308ff3f87926
Lucas Leão <Lucas Saraiva Leo>
2022-11-10 16:26:40 -0300

    Utilização do método find

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ url_base = url[0:19]
 print(url_base)
 url_parametros = url[20:]
 print(url_parametros)
+# usando o método find
+print()
+print('usando o método find')
+# print(url.find('b'))
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+print(url_base)
+
+url_parametros = url[indice_interrogacao+1:]
+print(url_parametros)
