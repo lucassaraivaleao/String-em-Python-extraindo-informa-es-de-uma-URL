c952de0c6d46a5a84d67658921ff686ef8eb3b72
Lucas Leão <Lucas Saraiva Leo>
2022-11-10 16:27:33 -0300

    Utilização do método len

--- main.py
+++ main.py
@@ -14,3 +14,11 @@ print(url_base)
 
 url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
+
+# utilizando o método len
+
+parametro_busca = "moedaOrigem"
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+valor = url_parametros[indice_valor:]
+print(valor)
