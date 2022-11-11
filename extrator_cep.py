669d3ec37664c16c3ff2c31d8e234b7b1d4cd021
Lucas Leão <Lucas Saraiva Leo>
2022-11-11 01:45:22 -0300

    utlizando quantificadores e identificadores para simplicar a expressão regEX

--- extrator_cep.py
+++ extrator_cep.py
@@ -1,8 +1,7 @@
 import re  # Regular Expression -- RegEx
 endereco = "Rua Coronel Romão Sampaio, 651, centro, Moreilândia/PE, 56150-000"
 # 5 dígitos + hífen (opcional) + 3 dígitos
-padrao = re.compile(
-    "[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]")
+padrao = re.compile("[0-9]{5}[-]{0,1}[0-9]{3}")
 busca = padrao.search(endereco)
 if busca:
     cep = busca.group()
