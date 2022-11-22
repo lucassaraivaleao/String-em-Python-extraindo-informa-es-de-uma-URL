6782b0ecf784995d9752bb0009d93c2b36061af1
Lucas Leão <Lucas Saraiva Leo>
2022-11-22 00:20:09 -0300

    inserido a função __len__ para contabilizar o tamanho da url

--- extrator_url.py
+++ extrator_url.py
@@ -42,10 +42,13 @@ class ExtratorURL:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
         return valor
 
+    def __len__(self):
+        return len(self.url)
+
 
 url = "bytebank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real"
-extrator_url=ExtratorURL(url)
-
-valor_quantidade=extrator_url.get_valor_parametro("quantidade")
+extrator_url = ExtratorURL(url)
+print('O tamanho da URL é', len(extrator_url))
+valor_quantidade = extrator_url.get_valor_parametro("quantidade")
 print(valor_quantidade)
 # extrator_url = ExtratorURL("  ")
